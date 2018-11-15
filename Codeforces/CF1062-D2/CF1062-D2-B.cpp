7ca311c021677f76d8d9eb0ada8907e2786c83c5
Ibraheem Tuffaha <bro.t.1996@gmail.com>
2018-11-15 09:05:18 +0200

    Fixed indentation

--- Codeforces/CF1062-D2/CF1062-D2-B.cpp
+++ Codeforces/CF1062-D2/CF1062-D2-B.cpp
@@ -1,11 +1,11 @@
 /*
-	Author:	Ibraheem Tuffaha
-	Idea:		- Find the frequency of the prime factors, the result will be
-						the multiplication of each prime factor once, and the number
-						of operations is ceil(log(max(frequency))), because to taking
-						the square root halves the powers of the prime factors.
-					- We should add 1 to the number of operations if not all powers
-						are equal or the max power is not a power of 2.
+  Author: Ibraheem Tuffaha
+  Idea:   - Find the frequency of the prime factors, the result will be
+            the multiplication of each prime factor once, and the number
+            of operations is ceil(log(max(frequency))), because to taking
+            the square root halves the powers of the prime factors.
+          - We should add 1 to the number of operations if not all powers
+            are equal or the max power is not a power of 2.
 */
 #include <bits/stdc++.h>
 typedef long long ll;
@@ -15,49 +15,49 @@ const int N = 100 + 5;
 int n, a[N];
 
 int Log2(int x) {
-	int y = 1, ret = -1;
-	while (y < x) ++ret, y *= 2;
-	return ret;
+  int y = 1, ret = -1;
+  while (y < x) ++ret, y *= 2;
+  return ret;
 }
 
 
 int main() {
-	scanf("%d", &n);
-	vector<int> pr;
-	vector<bool> is(n+10);
-	is[0] = is[1] = true;
-	for(int i=2; i<=n; ++i)
-		if(!is[i]) {
-			pr.push_back(i);
-			for(ll j=(ll)i*i; j<=n; j += i)
-				is[j] = true;
-		}
-	vector<int> v;
-	int r1 = 1;
-	int mx = -1;
-	for(auto x : pr) {
-		int cnt = 0, y = n;
-		while(y % x == 0) {
-			y /= x;
-			++cnt;
-		}
-		mx = max(mx, cnt);
-		if(cnt) {
-			v.push_back(cnt);
-			r1 *= x;
-		}
-	} 	
-	int r2 = 0;
-	bool ok = false;
-	for(auto x : v) {
-		if(x < mx)
-			ok = true;
-	}
-	if(mx > 1) {
-		r2 = Log2(mx)+1;
-		if((mx & (mx-1)) != 0)
-			ok = true;
-	}
-	printf("%d %d\n", r1, r2 + ok);
-	return 0;
+  scanf("%d", &n);
+  vector<int> pr;
+  vector<bool> is(n+10);
+  is[0] = is[1] = true;
+  for(int i=2; i<=n; ++i)
+    if(!is[i]) {
+      pr.push_back(i);
+      for(ll j=(ll)i*i; j<=n; j += i)
+        is[j] = true;
+    }
+  vector<int> v;
+  int r1 = 1;
+  int mx = -1;
+  for(auto x : pr) {
+    int cnt = 0, y = n;
+    while(y % x == 0) {
+      y /= x;
+      ++cnt;
+    }
+    mx = max(mx, cnt);
+    if(cnt) {
+      v.push_back(cnt);
+      r1 *= x;
+    }
+  }
+  int r2 = 0;
+  bool ok = false;
+  for(auto x : v) {
+    if(x < mx)
+      ok = true;
+  }
+  if(mx > 1) {
+    r2 = Log2(mx)+1;
+    if((mx & (mx-1)) != 0)
+      ok = true;
+  }
+  printf("%d %d\n", r1, r2 + ok);
+  return 0;
 }
--- Codeforces/CF1062-D2/CF1062-D2-C.cpp
+++ Codeforces/CF1062-D2/CF1062-D2-C.cpp
@@ -1,14 +1,14 @@
 /*
-	Author:	Ibraheem Tuffaha
-	Idea:		- Count the number of ones and the number of zeros in the range
-						using cumulative sum, it is always better to take the ones before
-						the zeros because at any moment, if the one cells have value x
-						then the zeros cells will have x-1, thus taking x will add x to 
-						all the remaining cells instead if x-1.
-					- Taking the ones first will give a sum of powers of 2
-					  -> x = sum(2^i) for i (0->ones-1) = (2^ones)-1
-					- Now the zeros have initial value x, so similarly the result of
-						the zeros is x * (2^zeros)-1
+  Author: Ibraheem Tuffaha
+  Idea:   - Count the number of ones and the number of zeros in the range
+            using cumulative sum, it is always better to take the ones before
+            the zeros because at any moment, if the one cells have value x
+            then the zeros cells will have x-1, thus taking x will add x to
+            all the remaining cells instead if x-1.
+          - Taking the ones first will give a sum of powers of 2
+            -> x = sum(2^i) for i (0->ones-1) = (2^ones)-1
+          - Now the zeros have initial value x, so similarly the result of
+            the zeros is x * (2^zeros)-1
 */
 #include <bits/stdc++.h>
 typedef long long ll;
@@ -21,23 +21,23 @@ char st[N];
 int power[N];
 
 int main() {
-	power[0] = 1;
-	for(int i=1; i<N; ++i)
-		power[i] = power[i-1] * 2LL % M;
-	scanf("%d%d %s", &n, &q, st+1);
-	for(int i=1; i<=n; ++i) {
-		a[i] = st[i] - '0';
-		a[i] += a[i-1];
-	}
-	int l, r;
-	int res = 0, one, zero;
-	while(q--) {
-		scanf("%d%d", &l, &r);
-		one = a[r] - a[l-1];
-		zero = r-l+1 - one;
-		int x = (power[one]-1LL + M) % M;
-		res = (ll)x * ((1LL + (power[zero]-1LL + M) % M) % M) % M;
-		printf("%d\n", res);
-	}
-	return 0;
+  power[0] = 1;
+  for(int i=1; i<N; ++i)
+    power[i] = power[i-1] * 2LL % M;
+  scanf("%d%d %s", &n, &q, st+1);
+  for(int i=1; i<=n; ++i) {
+    a[i] = st[i] - '0';
+    a[i] += a[i-1];
+  }
+  int l, r;
+  int res = 0, one, zero;
+  while(q--) {
+    scanf("%d%d", &l, &r);
+    one = a[r] - a[l-1];
+    zero = r-l+1 - one;
+    int x = (power[one]-1LL + M) % M;
+    res = (ll)x * ((1LL + (power[zero]-1LL + M) % M) % M) % M;
+    printf("%d\n", res);
+  }
+  return 0;
 }

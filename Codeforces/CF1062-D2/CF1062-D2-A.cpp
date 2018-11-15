7770fe61dead65eda8bc9db894968d9711679759
Ibraheem Tuffaha <bro.t.1996@gmail.com>
2018-11-15 08:43:32 +0200

    Added contest CF1062-D2

---
+++ Codeforces/CF1062-D2/CF1062-D2-A.cpp
@@ -0,0 +1,34 @@
+/*
+	Author:	Ibraheem Tuffaha
+	Idea:		- first trick to make implementation easy is to add 0
+						at the start and 1001 at the end.
+					- Let X be the longest consequtive subsequence, then the
+						result is X-2 because removing the start or the end gives
+						ambiguity
+*/
+#include <bits/stdc++.h>
+typedef long long ll;
+typedef long double ld;
+using namespace std;
+const int N = 100 + 5;
+int n, a[N];
+
+int main() {
+	scanf("%d", &n);
+	for(int i=2; i<=n+1; ++i)
+		scanf("%d", a+i);
+	a[0] = -100;
+	a[1] = 0;
+	a[n+2] = 1001;
+	a[n+3] = 1e4;
+	n += 3;
+	int res = 2;
+	for(int i=1, cnt=1; i<n; ++i) {
+		if(a[i-1]+1 == a[i])
+			++cnt;
+		else cnt = 1;
+		res = max(res, cnt);
+	}
+	printf("%d\n", res-2);
+	return 0;
+}
---
+++ Codeforces/CF1062-D2/CF1062-D2-D.cpp
@@ -0,0 +1,27 @@
+/*
+	Author:	Ibraheem Tuffaha
+	Idea:		- for every number i, if j = i*m (multiple of i) then
+						i -> j -> -i -> -j -> i transformations are always
+						valid, so for every j = i*m, add 4*m =4*j/i. 
+*/
+#include <bits/stdc++.h>
+typedef long long ll;
+typedef long double ld;
+using namespace std;
+const int N = 1000000 + 5, M = 1000000007;
+bool vis[N];
+int main() {
+	int n;
+	scanf("%d", &n);
+	ll res = 0;
+	for(int i=2; i<=n; ++i) {
+		
+		for(ll j = (ll)i+i; j<=n; j += i) {
+			res += 4LL*j/i;
+			vis[j] = true;
+		}
+
+	}
+	cout << res << endl;
+	return 0;
+}
